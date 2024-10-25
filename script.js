0670e626ab082ed58fe07e8a89ef7edc2267f89d
alpinzx19 <marksesi613@gmail.com>
2024-10-25 09:08:36 +0700

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Pesan Anda telah dikirim!');
+let score = 0;
+let balance = 0;
+
+document.getElementById('increase').addEventListener('click', function() {
+    score++;
+    document.getElementById('score').innerText = `Score: ${score}`;
+});
+
+document.getElementById('topupButton').addEventListener('click', function() {
+    const amount = parseInt(document.getElementById('topupAmount').value);
+    if (!isNaN(amount) && amount > 0) {
+        balance += amount;
+        document.getElementById('balance').innerText = `Balance: ${balance}`;
+        document.getElementById('topupAmount').value = '';
+    } else {
+        alert('Please enter a valid amount.');
+    }
 });
